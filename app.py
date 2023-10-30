5181bc269a6d88cfe1ca05102e31df781f63306f
F2U8WGF_thdgit <zaid_yosafath_deanda@homedepot.com>
2023-10-30 14:40:41 -0600

    fixes search function

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ client = get_mongo_client(config)
 db = client.certificados
 certificados_collection = db.certificados
 
+busqueda = st.text_input("Introduzca el nombre a buscar:")
+
+reiniciar = st.button("Reiniciar busqueda")
+
+if reiniciar:
+    busqueda = ""
+
 cols = st.columns(2)
 
 image_formats = ["png","jpg","jpeg"]
@@ -20,10 +27,8 @@ cols[0].subheader("Haga click en el botón para acceder a su certificado")
 
 global_list = os.listdir("Assets/Reconocimientos 87/")
 
-busqueda = st.text_input("Introduzca el nombre a buscar:")
-
 if busqueda:
-    global_list = [brand for brand in global_list if 'busqueda' in global_list]
+    global_list = [brand for brand in global_list if busqueda.upper() in brand]
 
 for file in global_list:
     with cols[0]:
@@ -34,7 +39,7 @@ for file in global_list:
         elif os.path.exists(f"Assets/Logos 87 donantes/{file[:-3]}{image_formats[2]}"):
             st.image(f"Assets/Logos 87 donantes/{file[:-3]}{image_formats[2]}")
         else:
-            st.write(file[:-3])
+            st.markdown(f"# {file[:-3]}")
         pwd_input = st.text_input("Introduce tú código", type="password",key=file)
 
         query = {
@@ -44,12 +49,13 @@ for file in global_list:
         doc = certificados_collection.find(query)
 
 
-        if pwd_input == doc[0]['pwd']:
+        if pwd_input:
+            if pwd_input == doc[0]['pwd']:
 
-            with open(f"Assets/Reconocimientos 87/{file}", "rb") as open_file:
-                btn = st.download_button(
-                        label="Acceder al certificado",
-                        data=open_file,
-                        file_name="certificado.pdf"
-                    )
+                with open(f"Assets/Reconocimientos 87/{file}", "rb") as open_file:
+                    btn = st.download_button(
+                            label="Acceder al certificado",
+                            data=open_file,
+                            file_name="certificado.pdf"
+                        )
         st.divider()
